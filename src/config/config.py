a19ff5018aa139c95c2cc4b827020bec2d5d2c26
Anton Tereshko <anton.tereshko1995@gmail.com>
2025-02-25 22:08:51 +0300

    load secret from cloud

--- src/config/config.py
+++ src/config/config.py
@@ -1,29 +1,52 @@
 from dotenv import load_dotenv
+from google.cloud import secretmanager
 import os
 
-if "secrets-production" in os.environ:
-    pass
-    # with open("projects/535413863315/secrets/the-secret-house-secret/versions/2/the-secret-house-secret") as f:
-    #     load_dotenv(stream=f)
-else:
-    ENV_FILE = "src/config/.env.debug" if os.getenv("ENV") == "debug" else "src/config/.env.production"
-    load_dotenv(ENV_FILE)
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-LOGTAIL_TOKEN = os.getenv("LOGTAIL_TOKEN")
-LOGTAIL_SOURCE = os.getenv("LOGTAIL_SOURCE")
-DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
-INFORM_CHAT_ID = os.getenv("INFORM_CHAT_ID")
-GPT_KEY = os.getenv("GPT_KEY")
-GPT_PROMPT = os.getenv("GPT_PROMPT")
-CALENDAR_ID = os.getenv("CALENDAR_ID")
-BANK_CARD_NUMBER = os.getenv("BANK_CARD_NUMBER")
-BANK_PHONE_NUMBER = os.getenv("BANK_PHONE_NUMBER")
-ADMINISTRATION_CONTACT = os.getenv("ADMINISTRATION_CONTACT")
 TELEGRAM_CONTACT = "https://t.me/the_secret_house"
 PERIOD_IN_MONTHS = 2
 MAX_PERIOD_FOR_GIFT_IN_MONTHS = 3
 MAX_PERIOD_FOR_SUBSCRIPTION_IN_MONTHS = 3
 PREPAYMENT = 80
 CLEANING_HOURS = 2
+
+def get_secret(project_id: str) -> secretmanager.GetSecretRequest:
+    client = secretmanager.SecretManagerServiceClient()
+    name = client.secret_path(project_id, "the-secret-house")
+    response = client.get_secret(request={"name": name})
+    if "automatic" in response.replication:
+        replication = "AUTOMATIC"
+    elif "user_managed" in response.replication:
+        replication = "MANAGED"
+    else:
+        raise Exception(f"Unknown replication {response.replication}")
+    
+    return response.payload.data.decode("UTF-8")
+
+if "secrets-production" in os.environ:
+    TELEGRAM_TOKEN = get_secret("TELEGRAM_TOKEN")
+    LOGTAIL_TOKEN = get_secret("LOGTAIL_TOKEN")
+    LOGTAIL_SOURCE = get_secret("LOGTAIL_SOURCE")
+    DATABASE_URL = get_secret("DATABASE_URL")
+    ADMIN_CHAT_ID = get_secret("ADMIN_CHAT_ID")
+    INFORM_CHAT_ID = get_secret("INFORM_CHAT_ID")
+    GPT_KEY = get_secret("GPT_KEY")
+    GPT_PROMPT = get_secret("GPT_PROMPT")
+    CALENDAR_ID = get_secret("CALENDAR_ID")
+    BANK_CARD_NUMBER = get_secret("BANK_CARD_NUMBER")
+    BANK_PHONE_NUMBER = get_secret("BANK_PHONE_NUMBER")
+    ADMINISTRATION_CONTACT = get_secret("ADMINISTRATION_CONTACT")
+else:
+    ENV_FILE = "src/config/.env.debug" if os.getenv("ENV") == "debug" else "src/config/.env.production"
+    load_dotenv(ENV_FILE)
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    LOGTAIL_TOKEN = os.getenv("LOGTAIL_TOKEN")
+    LOGTAIL_SOURCE = os.getenv("LOGTAIL_SOURCE")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
+    INFORM_CHAT_ID = os.getenv("INFORM_CHAT_ID")
+    GPT_KEY = os.getenv("GPT_KEY")
+    GPT_PROMPT = os.getenv("GPT_PROMPT")
+    CALENDAR_ID = os.getenv("CALENDAR_ID")
+    BANK_CARD_NUMBER = os.getenv("BANK_CARD_NUMBER")
+    BANK_PHONE_NUMBER = os.getenv("BANK_PHONE_NUMBER")
+    ADMINISTRATION_CONTACT = os.getenv("ADMINISTRATION_CONTACT")
