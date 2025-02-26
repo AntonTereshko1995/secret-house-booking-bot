24e4c7e4cc5f7bdb36a0e370a4041c15cee456d8
Anton Tereshko <anton.tereshko1995@gmail.com>
2025-02-26 20:43:59 +0300

    Update secret_manager_service.py

--- src/services/secret_manager_service.py
+++ src/services/secret_manager_service.py
@@ -3,7 +3,7 @@ from google.cloud import secretmanager
 
 @singleton
 class SecretManagerService:
-    def get_secret_by_dict(secret_id: str) -> dict:
+    def get_secret_by_dict(self, secret_id: str) -> dict:
         client = secretmanager.SecretManagerServiceClient()
         secret_path = f"projects/the-secret-house/secrets/{secret_id}/versions/latest"
         response = client.access_secret_version(request={"name": secret_path})
@@ -11,7 +11,7 @@ class SecretManagerService:
         secret_dict = dict(line.split("=", 1) for line in secret_string.splitlines() if "=" in line)
         return secret_dict
     
-    def get_secret(secret_id: str) -> str:
+    def get_secret(self, secret_id: str) -> str:
         client = secretmanager.SecretManagerServiceClient()
         secret_path = f"projects/the-secret-house/secrets/{secret_id}/versions/latest"
         response = client.access_secret_version(request={"name": secret_path})
