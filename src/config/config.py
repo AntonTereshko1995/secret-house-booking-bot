0f7ed66cdd007b52a1d72ff58a5e09139c8afa94
Anton Tereshko <anton.tereshko1995@gmail.com>
2025-02-26 16:14:33 +0300

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -10,27 +10,30 @@ PREPAYMENT = 80
 CLEANING_HOURS = 2
 
 # projects/535413863315/secrets/TELEGRAM_TOKEN
-def get_secret(secret_id: str) -> secretmanager.GetSecretRequest:
+def get_secret_value():
     client = secretmanager.SecretManagerServiceClient()
-    # name = client.secret_path("the-secret-house", secret_id)
-    # response = client.get_secret(request={"name": name})
-    name = f"projects/the-secret-house/secrets/{secret_id}/versions/latest"
-    response = client.access_secret_version(request={"name": name})
-    return response.payload.data.decode("UTF-8")
+    name = client.secret_path("the-secret-house", "the-secret-house-secret")
+    response = client.get_secret(request={"name": name})
+    # name = f"projects/the-secret-house/secrets/the-secret-house-secret/versions/latest"
+    # response = client.access_secret_version(request={"name": name})
+    secret_string = response.payload.data.decode("UTF-8")
+    secret_dict = dict(line.split("=", 1) for line in secret_string.splitlines() if "=" in line)
+    return secret_dict
 
 if "secrets-production" in os.environ:
-    TELEGRAM_TOKEN = get_secret("TELEGRAM_TOKEN")
-    LOGTAIL_TOKEN = get_secret("LOGTAIL_TOKEN")
-    LOGTAIL_SOURCE = get_secret("LOGTAIL_SOURCE")
-    DATABASE_URL = get_secret("DATABASE_URL")
-    ADMIN_CHAT_ID = get_secret("ADMIN_CHAT_ID")
-    INFORM_CHAT_ID = get_secret("INFORM_CHAT_ID")
-    GPT_KEY = get_secret("GPT_KEY")
-    GPT_PROMPT = get_secret("GPT_PROMPT")
-    CALENDAR_ID = get_secret("CALENDAR_ID")
-    BANK_CARD_NUMBER = get_secret("BANK_CARD_NUMBER")
-    BANK_PHONE_NUMBER = get_secret("BANK_PHONE_NUMBER")
-    ADMINISTRATION_CONTACT = get_secret("ADMINISTRATION_CONTACT")
+    secrets = get_secret_value()
+    TELEGRAM_TOKEN = secrets.get("TELEGRAM_TOKEN")
+    LOGTAIL_TOKEN = secrets.get("LOGTAIL_TOKEN")
+    LOGTAIL_SOURCE = secrets.get("LOGTAIL_SOURCE")
+    DATABASE_URL = secrets.get("DATABASE_URL")
+    ADMIN_CHAT_ID = secrets.get("ADMIN_CHAT_ID")
+    INFORM_CHAT_ID = secrets.get("INFORM_CHAT_ID")
+    GPT_KEY = secrets.get("GPT_KEY")
+    GPT_PROMPT = secrets.get("GPT_PROMPT")
+    CALENDAR_ID = secrets.get("CALENDAR_ID")
+    BANK_CARD_NUMBER = secrets.get("BANK_CARD_NUMBER")
+    BANK_PHONE_NUMBER = secrets.get("BANK_PHONE_NUMBER")
+    ADMINISTRATION_CONTACT = secrets.get("ADMINISTRATION_CONTACT")
 else:
     file = "src/config/.env.debug" if os.getenv("ENV") == "debug" else "src/config/.env.production"
     load_dotenv(file)
