78e05a1811e11d8285d62a082158eeb6565e9bea
Anton Tereshko <anton.tereshko1995@gmail.com>
2025-03-07 00:22:36 +0400

    Update secret_manager_service.py

--- src/services/secret_manager_service.py
+++ src/services/secret_manager_service.py
@@ -1,5 +1,10 @@
+import json
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from singleton_decorator import singleton
 from google.cloud import secretmanager
+from src.services.logger_service import LoggerService
 
 @singleton
 class SecretManagerService:
@@ -8,6 +13,7 @@ class SecretManagerService:
         secret_path = f"projects/the-secret-house/secrets/{secret_id}/versions/latest"
         response = client.access_secret_version(request={"name": secret_path})
         secret_string = response.payload.data.decode("UTF-8")
+        LoggerService.error(__name__, f"Secret data: {secret_string}")
         secret_dict = dict(line.split("=", 1) for line in secret_string.splitlines() if "=" in line)
         return secret_dict
     
