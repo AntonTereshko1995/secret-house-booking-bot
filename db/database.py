1d5fa8cec7c054b2265082adf97ae21c627da256
Anton Tereshko <anton.tereshko1995@gmail.com>
2025-03-07 14:30:46 +0400

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import logging
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from db.models.base import Base
 from db.models.user import UserBase
@@ -8,8 +9,11 @@ from db.models.gift import GiftBase
 from db.models.subscription import SubscriptionBase
 from sqlalchemy import create_engine
 from src.config.config import DATABASE_URL
-import os
 
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.error(f"BD path {DATABASE_URL}")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables() -> None:
