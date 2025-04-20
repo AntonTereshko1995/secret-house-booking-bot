7dc4548e369f9fd4f7ef2f43271bd3bb0257966f
Anton Tereshko <anton.tereshko1995@gmail.com>
2025-04-20 17:54:43 +0400

    Fix display answer from gpt

--- src/services/gpt_service.py
+++ src/services/gpt_service.py
@@ -3,7 +3,7 @@ import os
 from src.services.logger_service import LoggerService
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.services.database_service import DatabaseService
-from openai import OpenAI
+from openai import APITimeoutError, OpenAI
 from singleton_decorator import singleton
 from src.config.config import GPT_KEY, GPT_PROMPT
 
@@ -11,29 +11,39 @@ GPT_MODEL = "gpt-4o-mini"
 
 @singleton
 class GptService:
+
     def __init__(self):
-        self.client = OpenAI(api_key=GPT_KEY)
+        self.client = OpenAI(api_key=GPT_KEY, timeout=30)
         self.database_service = DatabaseService()
 
     async def generate_response(self, message: str) -> str:
-        try:
-            response = self.client.chat.completions.create(
-                model=GPT_MODEL,
-                temperature=0.7,
-                max_tokens=500,
-                messages=[
-                    {
-                        "role": "system",
-                        "content": GPT_PROMPT,
-                    },
-                    {
-                        "role": "user",
-                        "content": message,
-                    },
-                ])
+        retries = 3
+        while retries > 0:
+            try:
+                response = self.client.chat.completions.create(
+                    model=GPT_MODEL,
+                    temperature=0.7,
+                    max_tokens=500,
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": GPT_PROMPT,
+                        },
+                        {
+                            "role": "user",
+                            "content": message,
+                        },
+                    ])
 
-            return response.choices[0].message.content
-        except Exception as e:
-            print(f"Chat GPT Error: {e}")
-            LoggerService.error(__name__, f"generate_response", e)
-            return "Произошла ошибка. Попробуйте позже."
+                return response.choices[0].message.content
+            except APITimeoutError:
+                print("Chat GPT Timeout Error")
+                LoggerService.warning(__name__, "generate_response", "Chat GPT Timeout Error. Try again.")
+                retries -= 1
+                if retries == 0:
+                    LoggerService.error(__name__, "generate_response", "Chat GPT Timeout Error")
+                    return "Произошла ошибка. Попробуйте позже."
+            except Exception as e:
+                print(f"Chat GPT Error: {e}")
+                LoggerService.error(__name__, "generate_response", e)
+                return "Произошла ошибка. Попробуйте позже."
