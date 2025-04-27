b09a954f6a7bd36a8486c39778f5c42d18bd15ad
Anton Tereshko <anton.tereshko1995@gmail.com>
2025-04-28 00:32:43 +0400

    Fix text for date calendar

--- src/date_time_picker/calendar_picker.py
+++ src/date_time_picker/calendar_picker.py
@@ -1,3 +1,4 @@
+from matplotlib.dates import relativedelta
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ReplyKeyboardRemove
 from datetime import datetime, date, timedelta
 from src.helpers import string_helper, date_time_helper
@@ -38,14 +39,16 @@ def create_calendar(selected_date: date = None, min_date: date = None, max_date:
     #Last row - Buttons
     row=[]
     if selected_date.month != min_date.month:
-        row.append(InlineKeyboardButton("<", callback_data = create_callback_data("PREV-MONTH", selected_date.year, selected_date.month, day, prefix=callback_prefix)))
+        month_name = date_time_helper.get_month_name((selected_date - relativedelta(months=1)).month)
+        row.append(InlineKeyboardButton(f"⬅️ {month_name}", callback_data = create_callback_data("PREV-MONTH", selected_date.year, selected_date.month, day, prefix=callback_prefix)))
     else:
        row.append(InlineKeyboardButton(" ", callback_data = data_ignore))
 
     row.append(InlineKeyboardButton(action_text, callback_data = data_action))
 
     if selected_date.month != max_date.month:
-        row.append(InlineKeyboardButton(">", callback_data = create_callback_data("NEXT-MONTH", selected_date.year, selected_date.month, day, prefix=callback_prefix)))
+        month_name = date_time_helper.get_month_name((selected_date + relativedelta(months=1)).month)
+        row.append(InlineKeyboardButton(f"{month_name} ➡️", callback_data = create_callback_data("NEXT-MONTH", selected_date.year, selected_date.month, day, prefix=callback_prefix)))
     else:
        row.append(InlineKeyboardButton(" ", callback_data = data_ignore))
 
